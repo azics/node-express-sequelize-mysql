531fbe44661f4d5c3195c724328e5ef81b0d60e6
azics <giyasov.azamat@gmail.com>
2024-01-18 07:39:15 +0500

    fix

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors())
 
 app.use((req, res) => console.log("=======", process.env.PORT, req));
 
@@ -20,6 +17,6 @@ app.use('/api', require('./routes'));
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
